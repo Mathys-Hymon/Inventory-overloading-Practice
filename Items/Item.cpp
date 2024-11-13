ee9e01b04d5bc3b9cf7327e7b7b33a4be79309b1
Mathys Hymon <mathys.hymon@gmail.com>
2024-11-13 23:25:01 +0100

    [refractor] move items in folder

--- Items/Item.cpp
+++ Items/Item.cpp
@@ -2,6 +2,7 @@
 
 Item::Item(std::uint8_t type, int price, std::string name)
 {
+	// Constructor for the Item class
 	mName = name;
 	mType = type;
 	mPrice = price;
@@ -9,25 +10,30 @@ Item::Item(std::uint8_t type, int price, std::string name)
 
 std::uint8_t Item::GetType()
 {
+	// Returns the type of the item
 	return mType;
 }
 
 const std::string Item::GetName()
 {
+	// Returns the name of the item
 	return mName;
 }
 
 const bool Item::HasTag(std::uint8_t type)
 {
+	// Checks if the item has a specific tag
 	return mType == type;
 }
 
 void Item::addTag(std::uint8_t type)
 {
+	// Adds a tag to the item
 	mType += type;
 }
 
 void Item::removeTag(std::uint8_t type)
 {
+	// Removes a tag from the item
 	mType -= type;
 }
